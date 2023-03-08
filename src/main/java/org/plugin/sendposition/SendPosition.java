1f5c8c80d112bde7ab30374f221b42ed5b47eaf8
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 18:31:48 +0900

    v1.3.0a

--- src/main/java/org/plugin/sendposition/SendPosition.java
+++ src/main/java/org/plugin/sendposition/SendPosition.java
@@ -1,9 +1,6 @@
 package org.plugin.sendposition;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
@@ -12,10 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.ChatPaginator;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 public final class SendPosition extends JavaPlugin {
@@ -38,16 +32,19 @@ public final class SendPosition extends JavaPlugin {
             return super.onTabComplete(sender, command, alias, args);
         if (args.length == 1) {
             if (args[0].length() == 0) {
-                return Arrays.asList("subject", "description");
+                return Arrays.asList("global", "subject", "description");
             } else {
+                if ("global".startsWith(args[0])) {
+                    return Collections.singletonList("global");
+                }
                 if ("subject".startsWith(args[0])) {
                     return Collections.singletonList("subject");
                 } else if ("description".startsWith(args[0])) {
                     return Collections.singletonList("description");
                 }
-                if ("description".startsWith(args[2])) {
+                if ("description".startsWith(args[0])) {
                     return Collections.singletonList("description");
-                } else if ("subject".startsWith(args[2])) {
+                } else if ("subject".startsWith(args[0])) {
                     return Collections.singletonList("subject");
                 }
             }
@@ -55,24 +52,27 @@ public final class SendPosition extends JavaPlugin {
         return super.onTabComplete(sender, command, alias, args);
     }
 
+    public static void playallplayer() {
+        List<Player> players = (List<Player>) Bukkit.getOnlinePlayers();
+        for (Player player : players) {
+            player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
+        }
+    }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = null;
         if (sender instanceof Player) {
-            Player player = (Player) sender;
+            player = (Player) sender;
             String name = "name:" + ChatColor.BOLD + player.getName() + ChatColor.RESET;
             Location loc = player.getLocation();
             @NotNull String ddim = ((Player) sender).getWorld().getName();
-            String dim;
-            if (ddim.equals("world")) {
-                dim = ChatColor.DARK_GREEN + "OverWorld" + ChatColor.RESET;
-            } else if (ddim.equals("world_nether")) {
-                dim = ChatColor.DARK_RED + "The Nether" + ChatColor.RESET;
-            } else if (ddim.equals("world_the_end")) {
-                dim = ChatColor.DARK_PURPLE + "The End" + ChatColor.RESET;
-            } else {
-                dim = ChatColor.DARK_GRAY + ddim + ChatColor.RESET;
-            }
+            String dim = switch (ddim) {
+                case "world" -> ChatColor.DARK_GREEN + "OverWorld" + ChatColor.RESET;
+                case "world_nether" -> ChatColor.DARK_RED + "The Nether" + ChatColor.RESET;
+                case "world_the_end" -> ChatColor.DARK_PURPLE + "The End" + ChatColor.RESET;
+                default -> ChatColor.DARK_GRAY + ddim + ChatColor.RESET;
+            };
             double dx = loc.getX();
             double dy = loc.getY();
             double dz = loc.getZ();
@@ -80,31 +80,56 @@ public final class SendPosition extends JavaPlugin {
             double y = Math.floor(dy * 100) / 100;
             double z = Math.floor(dz * 100) / 100;
             if (args.length == 0) {
-                sender.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z);
+                player.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z);
                 player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
                 getLogger().info(name + " is at " + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z);
-                return true;
             } else {
                 if (args.length == 2 && args[0].equalsIgnoreCase("subject")) {
-                    sender.sendMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[1]);
+                    player.sendMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[1]);
                     player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
                     getLogger().info(name + " is at " + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[1]);
                 } else if (args.length == 4 && args[2].equalsIgnoreCase("description")) {
-                    sender.sendMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[1] + " description:" + args[3]);
+                    player.sendMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[1] + " description:" + args[3]);
                     player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
                     getLogger().info(name + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[1] + " description:" + args[3]);
                 } else if (args.length == 2 && args[0].equalsIgnoreCase("description")) {
-                    sender.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " description:" + args[1]);
+                    player.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " description:" + args[1]);
                     player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
                     getLogger().info(name + " dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " description:" + args[1]);
                 } else if (args.length == 4 && args[2].equalsIgnoreCase("subject")) {
-                    sender.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[3] + " description:" + args[1]);
+                    player.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[3] + " description:" + args[1]);
                     player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
                     getLogger().info(name + " dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[3] + " description" + args[1]);
+                }
+                if (args.length == 1 && args[0].equalsIgnoreCase("global")) {
+                    player.sendMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z);
+                    player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 2.0f, 1.0f);
+                    getLogger().info(name + " is at " + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " global");
                 } else {
-                    sender.sendMessage("that command cannot be executed");
+                    if (args.length == 3 && args[1].equalsIgnoreCase("subject")) {
+                        Bukkit.broadcastMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[2]);
+                        playallplayer();
+                        getLogger().info(name + " is at " + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[2] + " global");
+                    } else if (args.length == 5 && args[3].equalsIgnoreCase("description")) {
+                        Bukkit.broadcastMessage("[Send Position] " + name + " is at " + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[2] + " description:" + args[4]);
+                        playallplayer();
+                        getLogger().info(name + "dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[2] + " description:" + args[4] + " global");
+                    } else if (args.length == 3 && args[1].equalsIgnoreCase("description")) {
+                        Bukkit.broadcastMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " description:" + args[2]);
+                        playallplayer();
+                        getLogger().info(name + " dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " description:" + args[2] + " global");
+                    } else if (args.length == 5 && args[3].equalsIgnoreCase("subject")) {
+                        Bukkit.broadcastMessage("[Send Position] " + name + " dimension:" + dim + ChatColor.RED + " x:" + ChatColor.RESET + x + ChatColor.GREEN + " y:" + ChatColor.RESET + y + ChatColor.BLUE + " z:" + ChatColor.RESET + z + " subject:" + args[4] + " description:" + args[2]);
+                        playallplayer();
+                        getLogger().info(name + " dimension:" + dim + " x:" + x + " y:" + y + " z:" + z + " subject:" + args[4] + " description" + args[2] + " global");
+                    } else {
+                        player.sendMessage(ChatColor.DARK_RED + "that command cannot be executed");
+                    }
+                    return false;
+
                 }
 
+                return false;
             }
             return false;
         }
